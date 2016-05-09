928c7fd66272babe3b5d0698fb4d94425aa02606
root <vladb95z-soft@yandex.ru>
2016-05-09 18:12:43 +0300

    added a function for getted card information

--- main.js
+++ main.js
@@ -44,6 +44,12 @@ function mainCtrl(mainFactory, ipCookie, $timeout){
   };
 
   vm.getData = function(id){
+    if(vm.lists[id].showCards){
+      console.log(id);
+      mainFactory.getCard(id).then(function(data){
+        console.log(data);
+      });
+    }
     return vm.lists[id].showCards?vm.lists[id].cards:vm.lists[id].board;
   };
 
@@ -76,15 +82,32 @@ function mainCtrl(mainFactory, ipCookie, $timeout){
 
   var timeout = function(){
     getListsCards();
-    console.log(vm.trackList);
+    if(vm.trackList.length>0){
+      mainFactory.compareLists(vm.trackList[0].cards, vm.trackList[1].cards);
+    }
     myTimeOut = $timeout(timeout, 5000);
   }
 }
 
 angular.module('trelloApp').factory('mainFactory', mainFact);
 
-mainFact.$inject = ['$q', '$http', 'KEY', 'TOKEN'];
-function mainFact($q, $http, KEY, TOKEN){
+mainFact.$inject = ['$q', '$http', 'KEY', 'TOKEN', 'ipCookie'];
+function mainFact($q, $http, KEY, TOKEN, ipCookie){
+  var getNewCards = function(cards, cookieName){
+      var newCards = [];
+      var addFlag = false;
+      var lastCard = ipCookie(cookieName);
+
+      for(var i=0;i<cards.length;i++){
+        if(cards[i].id === lastCard.id){
+          addFlag = true;
+        }
+        if(addFlag){
+          newCards.push(cards[i]);
+        }
+      }
+      return newCards;
+  }
 
   var factory = {
     getBoards:function(boardId){
@@ -96,6 +119,7 @@ function mainFact($q, $http, KEY, TOKEN){
       });
       return defer.promise;
     },
+
     getLists:function(listId){
       var defer = $q.defer();
       $http.get('https://api.trello.com/1/lists/'+listId, {params:{cards:'all', key:KEY, token:TOKEN}}).then(function(data){
@@ -106,7 +130,79 @@ function mainFact($q, $http, KEY, TOKEN){
       return defer.promise;
     },
 
+    getCard: function(cardId){
+      var defer = $q.defer();
+      $http.get('https://api.trello.com/1/cards/'+cardId, { params: {
+        fields: 'due,idMembers,name,pos',
+        member_fields: 'fullName,username',
+        key: KEY,
+        token: TOKEN
+      }}).then(function(data){
 
+      }, function(data){
+        defer.resolve(data);
+      });
+      return defer.promise;
+    },
+
+    addOrUpdateCard:function(listId, cardObj){
+      if(cardObj === undefined){
+        return null;
+      }
+      var card = {
+        name: 'Test 2',
+        desc: "Test",
+        //pos: cardObj.pos,
+        idList: '56f3af1d4449dcaaa1c1fc6d',
+        urlSource:null
+      }
+      var defer = $q.defer();
+
+      $http.post('https://api.trello.com/1/cards?key='+KEY+'&token='+TOKEN, card,{
+              headers : {
+                'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+            }}).then(function(data){
+        //console.log(data);
+      },function(error){
+        //console.log(error);
+      })
+      return defer.promise;
+    },
+
+    compareLists:function(firstList, secondList){
+      var firstNewCard = [];
+      var secondNewCard = [];
+      if(firstList.length>0){
+        if(!ipCookie('lastCardDate_l1')){
+          ipCookie('lastCardDate_l1', JSON.stringify({
+            id:firstList[firstList.length-1].id,
+            date:firstList[firstList.length-1].dateLastActivity
+          }));
+        }else{
+          firstNewCard = getNewCards(firstList ,'lastCardDate_l1');
+          ipCookie('lastCardDate_l1', JSON.stringify({
+            id:firstNewCard[firstList.length-1].id,
+            date:firstNewCard[firstList.length-1].dateLastActivity
+          }));
+        }
+      }
+      if(secondList.length>0){
+        if(!ipCookie('lastCardDate_l2')){
+          ipCookie('lastCardDate_l2', JSON.stringify({
+            id:secondList[secondList.length-1].id,
+            date:secondList[secondList.length-1].dateLastActivity
+          }));
+        }else{
+          secondNewCard = getNewCards(secondList ,'lastCardDate_l2');
+          ipCookie('lastCardDate_l2', JSON.stringify({
+            id:secondNewCard[secondList.length-1].id,
+            date:secondNewCard[secondList.length-1].dateLastActivity
+          }));
+        }
+      }
+      console.log(firstNewCard);
+      console.log(secondNewCard);
+    }
   };
 
   return factory;
